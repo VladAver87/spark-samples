166fb893f8b9aa553f9df9e0d1e7fdcef8d0bdf2
Vlady <Averianov87@gmail.com>
2022-12-14 13:38:18 +0300

    Initial commit

---
+++ src/main/scala/rdd/Lab1ReduceByKey.scala
@@ -0,0 +1,12 @@
+package rdd
+
+
+object Lab1ReduceByKey extends App {
+  /*
+   * Lab1 - пример использование reduceByKey
+   * Посчитать общий объем всех заказов и какое кол-во раз был выполнен заказ,
+   * относительно каждого клиента.
+   * Итоговое множество содержит поля: order.customerID, sum(order.numberOfProduct), count(1)
+   * */
+
+}
