997cdd4013f657ebb9c1b2c6089f327d35a0e8cf
Vlady <Averianov87@gmail.com>
2022-12-23 08:57:32 +0300

    ref

---
+++ src/main/scala/rdd/model/Product.scala
@@ -0,0 +1,8 @@
+package rdd.model
+
+case class Product(
+                    id: Int,
+                    name: String,
+                    price: Double,
+                    numberOfProducts: Int
+                  )
